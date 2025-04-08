5d2c33e72fc72e0ebba3e61c6a108fd410594dc8
Egwau-Godfrey <egwaujunior@gmail.com>
2025-04-08 19:52:23 +0300

    Modified the MangaKatan Search

--- src/Webtoon/WebtoonI18NHelper.ts
+++ src/Webtoon/WebtoonI18NHelper.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import moment from "moment";
 import "moment/locale/fr";
 import "moment/locale/de";
